37326315ddf6bbd9c163a209f97edaf8bfdf2971
avelando <avelarrodrigues89@gmail.com>
2023-10-25 16:57:00 -0300

    Talvez eu tenha acabado, não sei

--- src/notificacoes/NotificacoesEmail.java
+++ src/notificacoes/NotificacoesEmail.java
@@ -1,10 +1,9 @@
 package notificacoes;
 
 public class NotificacoesEmail implements Notificacoes {
-    
+
     @Override
     public void enviaNotificacao(String tipo, double valor) {
-        // corpo/implementação do método
         System.out.println("enviando notificacao por email, " + tipo + ", Valor: " + valor);
     }
 }
